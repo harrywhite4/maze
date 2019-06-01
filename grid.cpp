39d875a094e07ceedb0ccb10c0a4e1552611ba38
Harry White <harry.white424@gmail.com>
2019-06-01 18:20:31 +1000

    Initial grid graph data structure

---
+++ grid.cpp
@@ -0,0 +1,80 @@
+#include <vector>
+
+#include "grid.h"
+
+GridGraph::GridGraph(unsigned int numRows, unsigned int numColumns) : 
+    numRows(numRows), 
+    numColumns(numColumns),
+    rightEdges(numColumns * numRows, false),
+    downEdges(numColumns * numRows, false) {
+}
+
+bool GridGraph::validNode(unsigned int node) {
+    return (node <= (numRows * numColumns));
+}
+
+unsigned int GridGraph::rowNumber(unsigned int node) {
+    return node / numColumns;
+}
+
+unsigned int GridGraph::columnNumber(unsigned int node) {
+    return node % numColumns;
+}
+
+bool GridGraph::hasEdge(unsigned int node, Direction dir) {
+    if (!validNode(node)) {
+        return false;
+    }
+
+    switch(dir) {
+    case Right:
+        return rightEdges[node];
+    case Left:
+        if (node-1 >= 0) {
+            return rightEdges[node-1];
+        }
+    case Down:
+        return downEdges[node];
+    case Up:
+        unsigned int rowAbove = rowNumber(node) - 1;
+        if (rowAbove >= 0) {
+            return downEdges[rowAbove + columnNumber(node)];
+        }
+    }
+    return false;
+}
+
+bool GridGraph::setEdge(unsigned int node, Direction dir, bool value) {
+    if (!validNode(node)) {
+        return false;
+    }
+
+    switch(dir) {
+    case Right:
+        rightEdges[node] = value;
+        return true;
+    case Left:
+        if (node-1 >= 0) {
+            rightEdges[node-1] = value;
+            return true;
+        }
+    case Down:
+        downEdges[node] = value;
+        return true;
+    case Up:
+        unsigned int rowAbove = rowNumber(node) - 1;
+        if (rowAbove >= 0) {
+            downEdges[rowAbove + columnNumber(node)] = value;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool GridGraph::addEdge(unsigned int node, Direction dir) {
+    return setEdge(node, dir, true);
+}
+
+bool GridGraph::removeEdge(unsigned int node, Direction dir) {
+    return setEdge(node, dir, false);
+}
---
+++ grid.h
@@ -0,0 +1,43 @@
+#ifndef MAZE_GRID_H_
+#define MAZE_GRID_H_
+
+#include <vector>
+
+enum Direction {Left, Right, Up, Down};
+
+class GridGraph {
+    
+    unsigned int numRows;
+    unsigned int numColumns;
+
+    std::vector<bool> rightEdges;
+    std::vector<bool> downEdges;
+
+    // Set edge from node in direction to value, return whether successful
+    bool setEdge(unsigned int node, Direction dir, bool value);
+
+    public:
+
+    GridGraph(unsigned int numRows, unsigned int rowWidth);
+
+    // Whether node number is valid
+    bool validNode(unsigned int node);
+
+    // 0 indexed row number
+    unsigned int rowNumber(unsigned int node);
+
+    // 0 indexed column number
+    unsigned int columnNumber(unsigned int node);
+
+    // Whether edge in this direction from node exists
+    bool hasEdge(unsigned int node, Direction dir);
+
+    // Add an edge from node in this direction, return whether edge was added
+    bool addEdge(unsigned int node, Direction dir);
+
+    // Remove an edge from node in this direction, return whether edge was removed
+    bool removeEdge(unsigned int node, Direction dir);
+
+};
+
+#endif
