eb3f9fc926d4b3e3a0818084db3a51ff7d291cb9
Harry White <harry.white424@gmail.com>
2019-06-03 22:18:55 +1000

    Use graphToImage in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,15 @@
 #include <iostream>
 
 #include "bitmap.hpp"
+#include "grid.hpp"
+#include "maze.hpp"
 
-const std::string usage = "Usage: bitmap [options] fname\nOptions:"
-                          "\n--color Use color image\n"
-                          "--bw Use black and white image\n"
+const std::string usage = "Usage: bitmap [options] fname\nOptions:\n"
                           "--help Print help\n";
 
 int main(int argc, char *argv[]) {
     // Variables
-    bool BlackWhite = true, success;
-    int imgWidth, imgHeight;
+    bool success;
     // Argument parsing
     std::vector<std::string> posArgs, flagArgs;
     std::string current;
@@ -27,11 +26,7 @@ int main(int argc, char *argv[]) {
 
     // Set based on flagArgs
     for (auto flag : flagArgs) {
-        if (flag == "--bw") {
-            BlackWhite = true;
-        } else if (flag == "--color") {
-            BlackWhite = false;
-        } else if (flag == "--help") {
+        if (flag == "--help") {
             std::cout << usage;
             exit(EXIT_SUCCESS);
         } else {
@@ -48,39 +43,12 @@ int main(int argc, char *argv[]) {
     std::string fname = posArgs[0];
 
     // Build data
-    if (!BlackWhite) {
-        imgWidth = 1000;
-        imgHeight = 1000;
-        std::vector<std::vector<Color24>> data(imgHeight, std::vector<Color24>(imgWidth));
-        for (int r = 0; r < imgHeight; ++r) {
-            for (int i = 0; i < imgWidth; ++i) {
-                if ((i % 20 > 9) || (r % 20 > 9)) {
-                    data[r][i].blue = 0;
-                } else {
-                    data[r][i].blue = 255;
-                }
-                data[r][i].red = i*r % 256;
-                data[r][i].green = 256 - (i*r % 256);
-            }
-        }
-        // Write bmp
-        std::cout << "Writing to " << fname << "\n";
-        success = writeBitmap24(fname, data);
-    } else {
-        imgWidth = 32;
-        imgHeight = 32;
-        std::vector<std::vector<bool>> data(imgHeight, std::vector<bool>(imgWidth, false));
-        for (int r = 0; r < imgHeight; ++r) {
-            for (int i = 0; i < imgWidth; ++i) {
-                if (i % 2 == 0) {
-                    data[r][i] = true;
-                }
-            }
-        }
-        // Write bmp
-        std::cout << "Writing to " << fname << "\n";
-        success = writeBitmapBW(fname, data);
-    }
+    GridGraph graph(3, 3);
+    graph.addEdge(0, Right);
+    graph.addEdge(3, Right);
+    graph.addEdge(6, Right);
+    auto image = graphToImage(graph);
+    success = writeBitmapBW(fname, image);
 
     if (!success) {
         std::cout << "Could not write to file\n";
