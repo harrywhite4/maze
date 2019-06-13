343b595dbf74da11b6a367627d759cab6e085c0d
Harry White <harry.white424@gmail.com>
2019-06-13 22:10:47 +1000

    Graph add edge tests

--- test/test.cpp
+++ test/test.cpp
@@ -19,6 +19,30 @@ TEST_F(GridGraphTest, TestNumRows) {
     ASSERT_EQ(graph.getNumRows(), 5);
 }
 
+TEST_F(GridGraphTest, TestAddUp) {
+    graph.addEdge(6, Up);
+    ASSERT_TRUE(graph.hasEdge(6, Up));
+    ASSERT_TRUE(graph.hasEdge(1, Down));
+}
+
+TEST_F(GridGraphTest, TestAddLeft) {
+    graph.addEdge(6, Left);
+    ASSERT_TRUE(graph.hasEdge(6, Left));
+    ASSERT_TRUE(graph.hasEdge(5, Right));
+}
+
+TEST_F(GridGraphTest, TestAddRight) {
+    graph.addEdge(6, Right);
+    ASSERT_TRUE(graph.hasEdge(6, Right));
+    ASSERT_TRUE(graph.hasEdge(7, Left));
+}
+
+TEST_F(GridGraphTest, TestAddDown) {
+    graph.addEdge(6, Up);
+    ASSERT_TRUE(graph.hasEdge(6, Down));
+    ASSERT_TRUE(graph.hasEdge(11, Up));
+}
+
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
