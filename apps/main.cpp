8cf07d93795a9752b106e0271aa36b8812aeb4d7
Harry White <harry.white424@gmail.com>
2020-03-28 17:55:01 +1100

    Catch parse exceptions thrown by cxxopts

--- apps/main.cpp
+++ apps/main.cpp
@@ -19,7 +19,7 @@ void validateDimension(int dimension, std::string name) {
 }
 
 int main(int argc, char *argv[]) {
-    // Argument parsing
+    // Argument definition
     auto options = cxxopts::Options("maze", "Generate mazes");
     options.add_options()
         ("o,output", "Output filename", cxxopts::value<std::string>()->default_value("maze.bmp"))
@@ -27,32 +27,47 @@ int main(int argc, char *argv[]) {
         ("h,height", "Maze height", cxxopts::value<int>()->default_value("50"))
         ("text", "Output maze as text to stdout", cxxopts::value<bool>()->default_value("false"))
         ("verbose", "Print detailed output", cxxopts::value<bool>()->default_value("false"))
-        ("help", "Print help")
+        ("help", "Print help", cxxopts::value<bool>()->default_value("false"))
     ;
     
-    auto result = options.parse(argc, argv);
+    // Variables
+    std::string fname;
+    int width, height;
+    bool help, text, verbose;
+    // Parse arguments
+    try {
+        cxxopts::ParseResult result = options.parse(argc, argv);
 
-    if (result.count("help")) {
+        fname = result["output"].as<std::string>();
+        width = result["width"].as<int>();
+        height = result["width"].as<int>();
+        help = result["help"].as<bool>();
+        text = result["text"].as<bool>();
+        verbose = result["verbose"].as<bool>();
+    } catch (cxxopts::OptionParseException& e) {
+        std::cerr << e.what() << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Validation
+    validateDimension(width, "width");
+    validateDimension(height, "height");
+
+    if (help) {
         std::cout << options.help() << std::endl;
         exit(EXIT_SUCCESS);
     }
-    std::string fname = result["output"].as<std::string>();
-    // Convert width / height & validate
-    int width = result["width"].as<int>();
-    int height = result["width"].as<int>();
-    validateDimension(width, "width");
-    validateDimension(height, "height");
 
     // Build data
     bool success;
     auto graph = GridGraph(height, width);
     lerwGraph(graph);
-    if (result.count("text")) {
+    if (text) {
         std::cout << graphToText(graph);
         success = true;
     } else {
         Image<bool> image = graphToImage(graph);
-        success = writeBitmapBW(fname, image, result["verbose"].as<bool>());
+        success = writeBitmapBW(fname, image, verbose);
     }
 
     if (!success) {
