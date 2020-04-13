151da16a79aeb29200878163540f98810cff9c8b
Harry White <harry.white424@gmail.com>
2020-04-13 16:35:48 +1000

    Tests for more grid graph functions
    Simple tests were added for edgeExists, nodeInDirection and
    getPossibleDirs

--- test/mazelib/grid_test.cpp
+++ test/mazelib/grid_test.cpp
@@ -42,3 +42,51 @@ TEST_F(GridGraphTest, TestAddDown) {
     ASSERT_TRUE(graph.hasEdge(6, mazelib::Down));
     ASSERT_TRUE(graph.hasEdge(11, mazelib::Up));
 }
+
+TEST_F(GridGraphTest, TestValidEdgeExists) {
+    ASSERT_TRUE(graph.edgeExists(0, mazelib::Right));
+}
+
+TEST_F(GridGraphTest, TestInvalidEdgeExists) {
+    ASSERT_FALSE(graph.edgeExists(0, mazelib::Left));
+}
+
+TEST_F(GridGraphTest, TestDirectionUp) {
+    ASSERT_EQ(graph.nodeInDirection(6, mazelib::Up), 1);
+}
+
+TEST_F(GridGraphTest, TestDirectionDown) {
+    ASSERT_EQ(graph.nodeInDirection(6, mazelib::Down), 11);
+}
+
+TEST_F(GridGraphTest, TestDirectionLeft) {
+    ASSERT_EQ(graph.nodeInDirection(6, mazelib::Left), 5);
+}
+
+TEST_F(GridGraphTest, TestDirectionRight) {
+    ASSERT_EQ(graph.nodeInDirection(6, mazelib::Right), 7);
+}
+
+TEST_F(GridGraphTest, TestCenterPossibleDirs) {
+    std::vector<mazelib::Direction> dirs;
+    graph.getPossibleDirs(dirs, 6);
+
+    std::vector<mazelib::Direction> expected = {
+        mazelib::Left,
+        mazelib::Right,
+        mazelib::Up,
+        mazelib::Down,
+    };
+    ASSERT_EQ(dirs, expected);
+}
+
+TEST_F(GridGraphTest, TestCornerPossibleDirs) {
+    std::vector<mazelib::Direction> dirs;
+    graph.getPossibleDirs(dirs, 4);
+
+    std::vector<mazelib::Direction> expected = {
+        mazelib::Left,
+        mazelib::Down,
+    };
+    ASSERT_EQ(dirs, expected);
+}
